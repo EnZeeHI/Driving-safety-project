cbba8a5f6399dc299073555df84dcbf14a59def9
Meap123 <korf.teun@gmail.com>
2019-01-20 18:47:15 +0100

    no message

---
+++ testVRnew/Assets/scripts/movementscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementscript : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+    public Transform[] target;
+    private int current;
+	
+	// Update is called once per frame
+	void Update () {
+        
+		if(transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else
+        {
+            current = (current + 1) % target.Length;
+        }
+        Vector3 direction = target[current].position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+    }
+}
