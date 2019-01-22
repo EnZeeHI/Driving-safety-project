4b7f6ac692e3e73fb9e70ba86831d4d91ae589f5
Meap123 <korf.teun@gmail.com>
2019-01-22 20:03:07 +0100

    no message

--- testVRnew/Assets/scripts/movementscript.cs
+++ testVRnew/Assets/scripts/movementscript.cs
@@ -6,21 +6,33 @@ using UnityEngine;
 public class movementscript : MonoBehaviour
 {
 
-    [Range(1,10)]
-    public float speed;
-    [Range(1,10)]
-    public float rotateSpeed;
-    public Transform[] target;
+    [SerializeField]
+    [Range(-200000000, 200000000)]
+    private float speed;
+    [SerializeField]
+    [Range(0.1f, 10f)]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private List<Transform> target;
     private int current;
+
     private Quaternion neededRotation;
+
+    
 
     private void Update ()
     {
+        Movement();
+    }
 
+    /// <summary>
+    /// Movement car
+    /// </summary>
+    private void Movement()
+    {
         neededRotation = Quaternion.LookRotation(target[current].position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, neededRotation, Time.deltaTime * rotateSpeed);
-
-
         if (transform.position != target[current].position)
         {
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
@@ -28,13 +40,13 @@ public class movementscript : MonoBehaviour
         }
         else
         {
-            current = (current + 1) % target.Length;
+            current = (current + 1) % target.Count;
         }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-          //  current = (current + 1) % target.Length;
-            //Debug.Log(current);
-        //}
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        current = (current + 1) % target.Count;
+        //        Debug.Log(current);
+        //    }
     }
 }
