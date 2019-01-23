1cd8aefd3e2fed61fad893638e0e1e5ab0d3b6e6
Meap123 <korf.teun@gmail.com>
2019-01-23 18:45:14 +0100

    no message

---
+++ testVRnew/Assets/Test_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Test_Movement : MonoBehaviour
+{
+    private float speed = 10f;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private List<Target> targetList;
+    private int currentTarget = 0;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (transform.position != targetList[currentTarget].transform.position)
+        {
+            Vector3 direction = new Vector3(transform.position.x + targetList[currentTarget].transform.position.x, transform.position.y + targetList[currentTarget].transform.position.y, transform.position.z + targetList[currentTarget].transform.position.z);
+            rb.velocity = direction * (speed * Time.deltaTime);
+        }
+        else
+        {
+            currentTarget = (currentTarget + 1) % targetList.Count;
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
--- testVRnew/Assets/scripts/movementscript.cs
+++ testVRnew/Assets/scripts/movementscript.cs
@@ -8,17 +8,17 @@ public class movementscript : MonoBehaviour
 
     [SerializeField]
     [Range(6, 18)]
-    private float speed;
+    public float speed;
     [SerializeField]
     [Range(0.1f, 10f)]
-    private float rotateSpeed;
+    public float rotateSpeed;
 
     [SerializeField]
     private List<Transform> target;
-    private int current;
+    public int current;
 
     private Quaternion neededRotation;
-
+    private Vector3 vel = Vector3.zero;
     
 
     private void Update ()
@@ -33,10 +33,17 @@ public class movementscript : MonoBehaviour
     {
         neededRotation = Quaternion.LookRotation(target[current].position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, neededRotation, Time.deltaTime * rotateSpeed);
+
+
+
+
         if (transform.position != target[current].position)
         {
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
+
+            //transform.position = Vector3.SmoothDamp(transform.position, target[current].position, ref vel, smoothTime);
+
         }
         else
         {
@@ -48,5 +55,15 @@ public class movementscript : MonoBehaviour
         //        current = (current + 1) % target.Count;
         //        Debug.Log(current);
         //    }
+
+
+
+
+
+
+
+
+
+
     }
 }
